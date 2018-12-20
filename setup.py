b563cc65c60b54a5c7967bd9c1eba939701cf4f9
QUANG TRAN <quangtran0698@gmail.com>
2018-12-20 21:17:40 +0700

    First commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fibheap",
+    version="0.0.1",
+    author="Quang Tran",
+    author_email="quangtran0698@gmail.com",
+    description="An implementation of Fibonacci heap",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
