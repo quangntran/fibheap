f6ab54a3dec0fe7f6fc2a7f14bd6af8999e45825
QUANG TRAN <quangtran0698@gmail.com>
2018-12-20 21:33:07 +0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="An implementation of Fibonacci heap",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/quangntran/fibheap.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
